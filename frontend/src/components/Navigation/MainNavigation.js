35f807382def2b2a59889d20a3692cd5a43054e8
riteshnus <ritesh.nus@gmail.com>
2019-05-12 17:15:56 +0800

    Add modal for event

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -21,9 +21,16 @@ const mainNavigation = props => (
             <li>
               <NavLink to="/events">Events</NavLink>
             </li>
-            {context.token && <li>
-              <NavLink to="/bookings">Bookings</NavLink>
-            </li> }
+            {context.token &&
+              <React.Fragment>
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li> 
+                <li>
+                  <button onClick={context.logout}>Logout</button>
+                </li> 
+              </React.Fragment>
+            }
           </ul>
         </div>
       </header>
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css';
 
 class EventPage extends Component {
+  state = {
+    creating: false
+  }
+
+  createEventHandler = () => {
+    this.setState({creating: true});
+  }
+
+  modalConfirmHandler = () => {
+    this.setState({creating: false});
+  }
+
+  modalCancelHandler = () => {
+    this.setState({creating: false});
+  }
   render() {
     return (
-      <div>
-        <h1>EventPage</h1>
-      </div>
+      <React.Fragment>
+        { this.state.creating && <Backdrop /> }
+        { this.state.creating && 
+          <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+          <p>Modal content</p>
+          </Modal> }
+          <div className="events-control">
+            <p>Share your own Events!</p>
+            <button className="btn" onClick={this.createEventHandler}>Create Event</button>
+          </div>
+      </React.Fragment>
     )
   }
 }
